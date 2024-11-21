edd9b59c3c0f62a9edacca994da29ea6867bcffc
arthurlazzari93 <arthur@lazzariseguros.com.br>
2024-11-20 22:47:07 -0300

    ajustes de DevOps

--- backend/sistema_comissoes/sistema_comissoes/settings.py
+++ backend/sistema_comissoes/sistema_comissoes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +90,15 @@ WSGI_APPLICATION = 'sistema_comissoes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
